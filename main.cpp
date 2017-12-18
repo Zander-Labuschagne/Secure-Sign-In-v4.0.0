fdecad89fb16ad6907d8523ff119efd3a5920506
Zander Labuschagne <zander.labuschagne@protonmail.ch>
2017-12-18 15:13:01 +0200

    after some consuting with Dirk Snyman

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 	SecureSignIn ssi;
 
 	std::cout << "Password: ";
-	std::string password;
+	std::string password;//verander na char
 	std::cin >> password;
 	std::cout << std::endl << "Key: ";
 	std::string key;
