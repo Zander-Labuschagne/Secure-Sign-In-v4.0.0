49e5580b403d6b3cfb3b6a3a9c1d93488b116e8e
Zander Labuschagne <zander.labuschagne@protonmail.ch>
2017-12-18 22:46:40 +0200

    Added hidden password input; removed buffer at user prompts; improved copy function for variable os support

--- main.cpp
+++ main.cpp
@@ -4,12 +4,25 @@
 
 int main()
 {
+	//Hide input - Also see getch() for C
+//	termios oldt;
+//	tcgetattr(STDIN_FILENO, &oldt);
+//	termios newt = oldt;
+//	newt.c_lflag &= ~ECHO;
+//	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	
+	set_echo(false);
+	
 	std::cout << "Password: ";
 	char password[256];
 	std::cin.getline(password, 256);
-	std::cout << "Key: ";
+	std::cout << std::endl << "Key: ";
 	char key[256];
 	std::cin.getline(key, 256);
+	std::cout << std::endl;
+	
+	set_echo(true);
+	set_buffer(false);
 	
 	std::cout << "Enter \'s\' to use the short version of the password." << std::endl;
 	std::cout << "Enter \'l\' to use the long version of the password. (Default)" << std::endl;
--- main.hpp
+++ main.hpp
@@ -1,10 +1,26 @@
 #include "SecureSignIn.hpp"
 #include <sstream>
 #include <string>
+#ifdef _WIN32
+	#include <windows.h>
+#else
+	#include <termios.h>
+	#include <unistd.h>
+#endif
 
 void display_password(const char* password);
-std::string exc(const char* cmd);
 std::string copy_password(const char* password);
+std::string copy_password_linux(const char* password);
+std::string copy_password_macos(const char* password);
+std::string copy_password_windows(const char* password);
+std::string exec(const char* tty);
+//tty tools, maybe build a library?
+void set_echo(bool enabled);
+void set_echo_unix(bool enabled);
+void set_echo_windows(bool enabled);
+void set_buffer(bool enabled);
+void set_buffer_unix(bool enabled);
+void set_buffer_windows(bool enabled);
 
 void display_password(const char* password)
 {
@@ -13,9 +29,114 @@ void display_password(const char* password)
 	std::cout << std::endl;
 }
 
-std::string exec(const char* cmd)
+std::string copy_password(const char* password)
 {
-	FILE* pipe = popen(cmd, "r");
+	#ifdef _WIN32 //both windows 32-bit and 64-bit; _WIN64 is only 64-bit
+		return copy_password_windows(password);
+	#elif __APPLE__ //All Apple devices; TARGET_OS_IPHONE for iOS device; TARGET_OS_MAC for macOS
+		return copy_password_macos(password);
+	#elif __linux__  //__unix__ // all unices not linux or macOS; defined(_POSIX_VERSION) for POSIX system
+		return copy_password_linux(password);
+	#endif
+}
+
+std::string copy_password_linux(const char* password)
+{
+	return copy_password_macos(password); //TODO: Review this
+}
+
+std::string copy_password_macos(const char* password)
+{
+	unsigned short xvix;
+	for(xvix = 0; *(password + xvix) != '\0'; xvix++);
+		
+	std::stringstream tty;
+	tty << "echo \"" << password << "\" | pbcopy";
+	
+	return exec(tty.str().c_str());
+}
+
+std::string copy_password_windows(const char* password)
+{
+	return copy_password_macos(password); //TODO: Review this
+
+	//#include "stdafx.h"
+	//#include "windows.h"
+	//#include "string.h"
+	//#include <direct.h>
+
+///////////////////////////////////////////////////////////////////////////	
+//	if ( !OpenClipboard() )
+//		{
+//			AfxMessageBox( _T("Cannot open the Clipboard") );
+//			return;
+//		}
+//		// Remove the current Clipboard contents
+//		if( !EmptyClipboard() )
+//		{
+//			AfxMessageBox( _T("Cannot empty the Clipboard") );
+//			return;
+//		}
+//		// Get the currently selected data
+//		HGLOBAL hGlob = GlobalAlloc(GMEM_FIXED, 64);
+//		strcpy_s((char*)hGlob, 64, "Current selection\r\n");
+//		// For the appropriate data formats...
+//		if ( ::SetClipboardData( CF_TEXT, hGlob ) == NULL )
+//		{
+//			CString msg;
+//			msg.Format(_T("Unable to set Clipboard data, error: %d"), GetLastError());
+//			AfxMessageBox( msg );
+//			CloseClipboard();
+//			GlobalFree(hGlob);
+//			return;
+//		}
+//		CloseClipboard();
+/////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////
+//const char* output = "Test";
+//const size_t len = strlen(output) + 1;
+//HGLOBAL hMem =  GlobalAlloc(GMEM_MOVEABLE, len);
+//memcpy(GlobalLock(hMem), output, len);
+//GlobalUnlock(hMem);
+//OpenClipboard(0);
+//EmptyClipboard();
+//SetClipboardData(CF_TEXT, hMem);
+//CloseClipboard();
+/////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////
+//#include <Windows.h>
+//char* LoadClipboard()
+//{
+//	static HANDLE clip;
+//	if(OpenClipboard(NULL))
+//	{
+//		clip = GetClipboardData(CF_TEXT);
+//		CloseClipboard();
+//	}
+//	return (char*) clip;
+//}
+//
+//void SaveClipboard(char* text)
+//{
+//	HGLOBAL global = GlobalAlloc(GMEM_FIXED,strlen(text) + 1); //text size + \0 character
+//	memcpy(global,text,strlen(text));  //text size + \0 character
+//	if(OpenClipboard(NULL))
+//	{
+//		EmptyClipboard();
+//		SetClipboardData(CF_TEXT,global);
+//		CloseClipboard();
+//	}
+//}
+///////////////////////////////////////////////////////////////////////////////////
+
+
+}
+
+std::string exec(const char* tty)
+{
+	FILE* pipe = popen(tty, "r");
 	if (!pipe) 
 		return "ERROR";
 	char buffer[128];
@@ -27,12 +148,70 @@ std::string exec(const char* cmd)
 	return result;
 }
 
-std::string copy_password(const char* password)
+void set_echo(bool enabled = true)
 {
-	unsigned short xvix;
-	for(xvix = 0; *(password + xvix) != '\0'; xvix++);
-	
-	std::stringstream cmd;
-	cmd << "echo \"" << password << "\" | pbcopy";
-	return exec(cmd.str().c_str());
+	#ifdef __unix__
+		set_echo_unix(enabled);
+	#else
+		set_echo_windows(enabled);
+	#endif
 }
+
+#ifdef __unix__ //Inlcudes Apple(macOS, iOS), Linux, UNIX
+void set_echo_unix(bool enabled = true)
+{
+	struct termios tty;
+	tcgetattr(STDIN_FILENO, &tty);
+	
+	if(!enabled)
+		tty.c_lflag &= ~ECHO;
+	else
+		tty.c_lflag |= ECHO;
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &tty);
+}
+#elif _WIN32
+void set_echo_windows(bool enabled = true)
+{
+	HANDLE std_handle = GetStdHandle(STD_INPUT_HANDLE); 
+	DWORD mode;
+	GetConsoleMode(std_handle, &mode);
+
+	if(!enabled)
+		mode &= ~ENABLE_ECHO_INPUT;
+	else
+		mode |= ENABLE_ECHO_INPUT;
+
+	SetConsoleMode(std_handle, mode);
+}
+#endif
+
+void set_buffer(bool enabled)
+{
+	#ifdef __unix__
+		set_buffer_unix(enabled);
+	#elif _WIN32
+		set_buffer_windows(enabled);
+	#endif
+}
+
+#ifdef __unix__ //Inlcudes Apple(macOS, iOS), Linux, UNIX
+void set_buffer_unix(bool enabled)
+{
+	struct termios tty;
+	tcgetattr(STDIN_FILENO, &tty); //get the current terminal I/O structure
+	
+	if(!enabled)
+		tty.c_lflag &= ~ICANON; //Manipulate the flag bits to do what you want it to do
+	else
+		tty.c_lflag |= ICANON; //Manipulate the flag bits to do what you want it to do
+			
+	tcsetattr(STDIN_FILENO, TSCANOW, &tty); //Apply the new settings
+}
+#elif _WIN32
+void set_buffer_windows(bool enabled)
+{
+	set_buffer_unix(enabled); //TODO: Review this
+}
+#endif
+
