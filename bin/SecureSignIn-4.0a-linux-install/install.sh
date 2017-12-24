582aad61ab6de90ce2e5f7e7780faa15bd052397
Zander Labuschagne <zander.labuschagne@protonmail.ch>
2017-12-24 14:57:21 +0200

    Created Linux and macOS installers

---
+++ bin/SecureSignIn-4.0a-linux-install/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir /opt/Cryogen
+cp SecureSignIn-4.0a-linux /opt/Cryogen/
+touch /usr/bin/ssi
+echo "#!/bin/bash" >> /usr/bin/ssi
+echo "/opt/Cryogen/SecureSignIn-4.0a-linux" >> /usr/bin/ssi
