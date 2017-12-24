1b4dbffd445f1ea4d1a69ab1567e567ccd600720
Zander Labuschagne <zander.labuschagne@protonmail.ch>
2017-12-24 13:40:55 +0200

    Removed global vars

--- src/X11_clipboard.c
+++ src/X11_clipboard.c
@@ -8,10 +8,16 @@
 	 */
 	void copy(const char *text, long seconds_active)
 	{
+		Display *display = XOpenDisplay(0);
+		Atom UTF8 = XInternAtom(display, "UTF8_STRING", 1);
+		const Atom XA_STRING = 31;
 		int size = strlen(text);
 		if (UTF8 == None)
 			UTF8 = XA_STRING;
 
+		const Atom SELECTION = XInternAtom(display, "CLIPBOARD", 0);
+		const int N = DefaultScreen(display);
+		Window window = XCreateSimpleWindow(display, RootWindow(display, N), 0, 0, 1, 1, 0, BlackPixel(display, N), WhitePixel(display, N));
 		XSetSelectionOwner(display, SELECTION, window, 0);
 		if (XGetSelectionOwner(display, SELECTION) != window)
 			return;
@@ -19,6 +25,7 @@
 		time_t end = time(NULL) + seconds_active; //declare end time equal to current time + seconds_active seconds
 		while (time(NULL) <= end) { // execute while current time is less than end time
 			XNextEvent(display, &event);
+			XEvent event;
 			switch (event.type) {
 			case SelectionRequest:
 				if (event.xselectionrequest.selection != SELECTION)
@@ -35,6 +42,7 @@
 				xse.time = xsre->time;
 				xse.target = xsre->target;
 				xse.property = xsre->property;
+				Atom atom_targets = XInternAtom(display, "TARGETS", 0);
 				if (xse.target == atom_targets)
 					r = XChangeProperty(xse.display, xse.requestor, xse.property, XA_ATOM, 32, PropModeReplace, (unsigned char*)&UTF8, 1);
 				else if (xse.target == XA_STRING || xse.target == ATOM_TEXT)
@@ -56,14 +64,21 @@
 	 */
 	char *paste()
 	{
+		Display *display = XOpenDisplay(0);
+		Atom UTF8 = XInternAtom(display, "UTF8_STRING", 1);
+		const Atom XA_STRING = 31;
 		Atom atom;
 		if (UTF8 != None)
 			atom = UTF8;
 		if (!atom)
 			atom = XA_STRING;
 		const Atom XSEL_DATA = XInternAtom(display, "XSEL_DATA", 0);
+		const Atom SELECTION = XInternAtom(display, "CLIPBOARD", 0);
+		const int N = DefaultScreen(display);
+		Window window = XCreateSimpleWindow(display, RootWindow(display, N), 0, 0, 1, 1, 0, BlackPixel(display, N), WhitePixel(display, N));
 		XConvertSelection(display, SELECTION, atom, XSEL_DATA, window, CurrentTime);
 		XSync(display, 0);
+		XEvent event;
 		XNextEvent(display, &event);
 		char *clipboard = 0;
 		switch (event.type) {
@@ -75,6 +90,7 @@
 				int format;
 				unsigned long n;
 				unsigned long size;
+				Atom atom_targets = XInternAtom(display, "TARGETS", 0);
 				XGetWindowProperty(event.xselection.display, event.xselection.requestor, event.xselection.property, 0L, (~0L), 0, AnyPropertyType, &atom_targets, &format, &size, &n, (unsigned char**)&data);
 				if (atom_targets == UTF8 || atom_targets == XA_STRING) {
 					clipboard = strndup(data, size);
--- src/X11_clipboard.h
+++ src/X11_clipboard.h
@@ -1,12 +1,12 @@
 /*
- * X11_clipboard: class containing C code to use the main clipboard object of X11.
+ * X11_clipboard: C functions to use the main clipboard object of X11.
  * 90% of this code is borrowed from exebook's barebones implementation: https://github.com/exebook/x11clipboard
  * I have just added abstraction by merging everything into two simple and easy to use functions in one C++ class for my own use.
  *
  * Author:
  *	Zander Labuschagne <zander.labuschagne@protonmail.ch>
  *
- * To compile add -X11 argument to g++ to include the X11 libraries
+ * To compile add -X11 argument to gcc to include the X11 libraries
  * This should only be compatible with Linux systems, if not please inform me.
  * I am still learning C++ so if anything is unacceptable or a violation to some standards please inform me.
 */
@@ -18,13 +18,4 @@
 
 	void copy(const char *text, long seconds_active);
 	char *paste();
-
-	Display *display = XOpenDisplay(0);
-	const int N = DefaultScreen(display);
-	Window window = XCreateSimpleWindow(display, RootWindow(display, N), 0, 0, 1, 1, 0, BlackPixel(display, N), WhitePixel(display, N));
-	XEvent event;
-	Atom atom_targets = XInternAtom(display, "TARGETS", 0);
-	Atom UTF8 = XInternAtom(display, "UTF8_STRING", 1);
-	const Atom XA_STRING = 31;
-	const Atom SELECTION = XInternAtom(display, "CLIPBOARD", 0);
 #endif
