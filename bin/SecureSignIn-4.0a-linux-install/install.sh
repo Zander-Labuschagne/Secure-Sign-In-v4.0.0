80b0f70d081078344e6a37530eb91585582ff647
Zander Labuschagne <zander.labuschagne@protonmail.ch>
2017-12-24 15:23:30 +0200

    Compressed installers

--- bin/SecureSignIn-4.0a-linux-install/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir /opt/Cryogen
-cp SecureSignIn-4.0a-linux /opt/Cryogen/
-touch /usr/bin/ssi
-echo "#!/bin/bash" > /usr/bin/ssi
-echo "/opt/Cryogen/SecureSignIn-4.0a-linux" > /usr/bin/ssi
-chmod +x /usr/bin/ssi
