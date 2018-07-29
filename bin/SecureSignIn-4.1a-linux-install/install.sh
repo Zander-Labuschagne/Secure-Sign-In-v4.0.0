fa22d30f34e620071a9f3336030f39953d10e49a
Zander-Labuschagne <Zander.Labuschagne@protonmail.ch>
2018-07-29 21:12:50 +0200

    Updated version number

--- bin/SecureSignIn-4.1a-linux-install/install.sh
+++ bin/SecureSignIn-4.1a-linux-install/install.sh
@@ -4,6 +4,6 @@ if [ -f /usr/bin/ssi ]; then
 	rm /usr/bin/ssi
 	echo "A version of Secure Sign In detected and removed."
 fi
-cp SecureSignIn-4.1a-linux /usr/bin/ssi
+cp SecureSignIn-4.1.1a-linux /usr/bin/ssi
 chmod +x /usr/bin/ssi
 echo "Secure Sign In v4.1.1a successfully installed."
